a5219779888d7cb5d8393c1dbc2fd7ab76bb32c8
sabbirRashed <srsabbirrahman12@gmail.com>
2026-04-29 00:32:25 +0600

    modified fetch method and fetch foods using useState for client component inside the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,28 @@
+'use client'
 import FoodCard from '@/components/FoodCard';
 import NoFoodsCard from '@/components/NoFoodsCard';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const HomePage = async () => {
-  const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/foods");
-  const data = await res.json();
-  const foods = data.data;
+const HomePage = () => {
+  const [foods, setFoods] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  
+console.log(searchInput)
+  useEffect(() => {
+    const loadFoods = async () => {
+      const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/foods");
+      const data = await res.json();
+      setFoods(data.data)
+    };
+    
+    loadFoods();
+  }, [])
 
 
   return (
     <div className='mt-20  mx-20'>
+      {/*home page heading */}
       <h2 className='flex justify-center items-end gap-2 font-poppins'>
         <span className='text-2xl md:text-4xl font-bold'>Discover Delicious Foods</span>
         <Image src='/chicken_fry.png'
@@ -20,11 +32,22 @@ const HomePage = async () => {
       </h2>
 
 
+      {/* search and filter section */}
       <div className='flex items-center gap-2 my-10 p-4 rounded-lg bg-base-200 border border-base-300'>
 
         <form className='flex w-full gap-2'>
-          <input type="text" name="text" id="" placeholder='Search food...' className='px-4 py-2 bg-base-300 w-[80%] flex-1 rounded-lg' />
-          <input type="submit" value="Search" className='p-2 bg-amber-600 px-6 rounded-lg' />
+          <input
+            type="text"
+            name="text"
+            value={searchInput}
+            onChange={e => {setSearchInput(e.target.value)}}
+            id=""
+            placeholder='Search food...'
+            className='px-4 py-2 bg-base-300 w-[80%] flex-1 rounded-lg' />
+          <input
+            type="submit"
+            value="Search"
+            className='p-2 bg-amber-600 px-6 rounded-lg' />
         </form>
 
         <select name="dropdown" id="" className='px-4 py-2 bg-base-300 rounded-lg'>
@@ -38,9 +61,9 @@ const HomePage = async () => {
         </select>
       </div>
 
-     
 
 
+      {/* food cards */}
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5'>
         {
           foods.map(food => {
